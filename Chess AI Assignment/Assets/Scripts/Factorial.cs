8e4095e9d364499ac2e28464a650622e0faf982b
TerrenceChan <roxes2112@yahoo.com>
2018-03-29 01:53:28 +0800

    *Worked on recursive functions.
    *Added factor checking and failed prime number checking.

---
+++ Chess AI Assignment/Assets/Scripts/Factorial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factorial : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(CountFactorial(0));
+    }
+
+    void Update()
+    {
+
+    }
+
+    public int CountFactorial(int value)
+    {
+        if (value <= 0)
+        {
+            return value;
+        }
+        else
+        {
+            return (value * CountFactorial(value - 1));
+        }
+    }
+}
---
+++ Chess AI Assignment/Assets/Scripts/PrimeNumber.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimeNumber : MonoBehaviour
+{
+    int amount = 100;
+
+    void Start()
+    {
+        for (int i = 0; i < amount; ++i)
+        {
+            CheckPrime(3);
+        }
+    }
+
+    bool CheckPrime(int value)
+    {
+        if (CheckPrime(value))
+        {
+            Debug.Log(value + "is a prime number.");
+            return CheckPrime(value + 1);
+        }
+        else
+        {
+            Debug.Log(value + "is not a prime number.");
+            return false;
+        }
+
+    }
+}
